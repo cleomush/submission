da04723f393d58a0d1fd3f0c2a9e7cab296782c5
Mubeen <khanmubeen101@gmail.com>
2017-11-10 20:37:27 +0200

    i dont have my results from last year so i just put in marks

---
+++ Mubeen 216213177/Khan216213177.R
@@ -0,0 +1,11 @@
+data=readxl::read_xlsx(file.choose())
+data
+summary(data)
+hist(data$`Term 5`,col="blue")
+boxplot(data$`Term 4`)
+library(reshape2)
+Marks=melt(data)
+colnames(Marks)=c("Subjects","Terms","Mark")
+head(Marks)
+library(ggplot2)
+ggplot(Marks,aes(x=Terms,y=Mark))+geom_point()
